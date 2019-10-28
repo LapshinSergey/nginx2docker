66e4ddad2ff1c9cb251a27161cd2c3134ae5dab2
Lapshin Sergey <sergey@lapshin.pro>
2019-10-28 15:26:03 +0300

    bug fix

--- nginx2docker.py
+++ nginx2docker.py
@@ -92,7 +92,11 @@ if sys.argv[1] == 'add-to-pool':
     if (APP_SSL == 'True'):
       subprocess.call(['sudo','/usr/bin/certbot', '-n', '-d', server_name, '--nginx', '--redirect'])
       if (APP_HTTP2 == 'True'):
-        subprocess.call(['sudo','sed', '-i.bak', '-e', '\'s/listen 443 ssl;/listen 443 ssl http2;/g\'', NGINX_CONFD_DIR + server_name + '.autopool.conf'])
+        try:
+          subprocess.check_call(['sudo','sed', '-i.bak', '-e', '\'s/listen 443 ssl;/listen 443 ssl http2;/g\'', NGINX_CONFD_DIR + server_name + '.autopool.conf'])
+        except subprocess.CalledProcessError:
+          print ('')
+          sys.exit()
     print ('Nginx reload .... ')
     subprocess.call(['sudo',"systemctl", "restart", "nginx"])
     print ('You domain? click here: https://'+server_name)
