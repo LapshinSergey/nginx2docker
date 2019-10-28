d970207294384fe0a706445c43dec61c31e28dc1
Lapshin Sergey <sergey@lapshin.pro>
2019-10-28 13:47:44 +0300

    bug fix

--- nginx2docker.py
+++ nginx2docker.py
@@ -87,7 +87,7 @@ if sys.argv[1] == 'add-to-pool':
                                         container_port = free_port,
                                         container_addr = '127.0.0.1'))
     if (APP_SSL == 'True'):
-      subprocess.call(['sudo','/usr/bin/certbot', '-n','-d ',server_name,'--nginx','--redirect'])
+      subprocess.call(['sudo','/usr/bin/certbot', '-n', '-d', server_name, '--nginx', '--redirect'])
     print ('Nginx reload .... ')
     subprocess.call(['sudo',"systemctl", "restart", "nginx"])
     print ('You domain? click here: https://'+server_name)
